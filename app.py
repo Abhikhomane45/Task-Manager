1fcd57e07d729fe4f846681ce632fc0aebe45257
Abhikhomane45 <abhikhomane123@gmail.com>
2025-07-14 12:02:10 +0530

    i have created the app.py and my new Project

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+
+@app.route("/login", methods=['POST'])
+def submit():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    return f"Username: {username},your Password is : {password}"
+
+if __name__=="__main__":
+    app.run(debug=True, port=8000)
