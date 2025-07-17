8ba932ef865579f2680ab19ebe867671a3aaceda
Abhikhomane45 <abhikhomane123@gmail.com>
2025-07-17 21:20:57 +0530

    created datbase

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy 
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    desc = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:    
+        return f"{self.sno} - {self.title}"
 
 @app.route("/")
 def hello_world():
@@ -10,11 +24,9 @@ def hello_world():
 def submit():
     username = request.form.get('username')
     password = request.form.get('password')
-    return f"Username: {username},your Password is : {password}"
+    return f"Username: {username}, your Password is: {password}"
 
-if __name__=="__main__":
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()  # ✅ This line fixes the issue
     app.run(debug=True, port=8000)
-    
-
-
-
