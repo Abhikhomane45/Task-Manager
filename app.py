08baba976f9b5a5097298bca0908dc8221b2be0a
Abhikhomane45 <abhikhomane45@outlook.com>
2025-07-17 21:46:43 +0530

    created database

--- app.py
+++ app.py
@@ -28,5 +28,6 @@ def submit():
 
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all()  # created the database 
+        db.create_all()  
         app.run(debug=True, port=8000)
+print("gell00")
